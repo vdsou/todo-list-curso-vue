732ea9d496ef74622fe65de4f05f870b2f1c3089
vdsou <vdsouza@outlook.com>
2022-04-08 16:44:49 -0300

    improve some styling

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,6 +1,6 @@
 <template>
-  <form action="">
-    <input type="text" placeholder="Nova tarefa" />
+  <form action="" class="new-task-form">
+    <input type="text" placeholder="Nova tarefa?" />
     <button>+</button>
   </form>
 </template>
@@ -9,4 +9,26 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.new-task-form {
+  margin: 20px 0;
+  border: 1px solid #fff;
+  display: flex;
+  width: 400px;
+}
+input {
+  font-size: 18px;
+  width: 90%;
+  height: 100%;
+  border: none;
+  overflow: hidden;
+  padding: 15px;
+}
+button {
+  font-size: 18px;
+  cursor: pointer;
+  width: 10%;
+  border: none;
+  height: 100%;
+}
+</style>
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="progress-bar">
-    <div class="progress-percent">0%</div>
+    <span> 100% </span>
+    <div class="progress-percent"></div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-  .progress-bar{
-    width: 90%;
-    border: 1px solid #fff;
-    border-radius: 5px;
-    overflow: hidden;
-  }
-  .progress-percent{
-    text-align: center;
-    background: #26de81;
-  }
+span {
+  position: absolute;
+  top: 10%;
+  left: 49%;
+  font-size: 18px;
+  font-weight: 600;
+}
+.progress-bar {
+  position: relative;
+  width: 70%;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  overflow: hidden;
+  height: 30px;
+}
+.progress-percent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  /* width: 0%; */
+  background: #26de81;
+}
 </style>
