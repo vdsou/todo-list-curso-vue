d00d2d18c06a5d452d0cdeeea0037f8bf426e8e1
vdsou <vdsouza@outlook.com>
2022-04-08 15:51:48 -0300

    add ProgressBar component to the project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+    <ProgressBar />
 	</div>
 </template>
 
 <script>
+import ProgressBar from "@/components/ProgressBar.vue"
 export default {
-
+  components: {ProgressBar}
 }
 </script>
 
 <style>
+  *{
+    box-sizing: border-box;
+  }
 	body {
 		font-family: 'Lato', sans-serif;
 		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="progress-bar">
+    <div class="progress-percent">0%</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .progress-bar{
+    width: 90%;
+    border: 1px solid #fff;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .progress-percent{
+    text-align: center;
+    background: #26de81;
+  }
+</style>
